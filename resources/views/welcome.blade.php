96367fa58bae6e55108137bf7fdfdd24adfff495
Jeff Puckett <jeff@jeffpuckett.com>
2016-08-18 22:01:22 -0500

    use video.js

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,22 +1,33 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Laravel</title>
 
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+        <link href="http://vjs.zencdn.net/5.8.8/video-js.css" rel="stylesheet">
 
-        <style>
-          video {
-            width: 100%;
-          }
-        </style>
+        <title>Laravel Video.js</title>
 
     </head>
     <body>
+
         <!--<video controls autoplay>-->
-        <video controls preload="auto" poster="http://vjs.zencdn.net/v/oceans.png">
-          <source src="http://vjs.zencdn.net/v/oceans.mp4" type="video/mp4"></source>
-          <p>not supported</p>
+        <video
+          controls
+          preload="auto"
+          poster="http://vjs.zencdn.net/v/oceans.png"
+          class="video-js vjs-big-play-centered"
+          data-setup="{}"
+        >
+
+          <source src="http://vjs.zencdn.net/v/oceans.mp4" type="video/mp4"/>
+          <p class="vjs-no-js">
+            To view this video please enable JavaScript, and consider upgrading to a web browser that
+            <a href="http://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a>
+          </p>
+
         </video>
+
+        <script src="http://vjs.zencdn.net/5.8.8/video.js"></script>
+
     </body>
 </html>
